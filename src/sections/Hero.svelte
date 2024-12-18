8726ef2541979d2da3bcf5f67a46b1ab292bddab
Mykhaylo <63978197+sargon17@users.noreply.github.com>
2024-12-18 22:44:41 +0100

    Add Hero section: create a new Svelte component for the landing page featuring a prominent title, mockup image, and a call-to-action button

---
+++ src/sections/Hero.svelte
@@ -0,0 +1,42 @@
+
+<script lang="ts">
+    import Button from '../components/ui/Button.svelte';
+import mockupImage from '../assets/mockup.png';
+
+</script>
+
+<div>
+    <div class="flex flex-col items-center justify-center min-h-screen">
+        <div class="mt-40 mb-20">
+            <div class="flex flex-col items-center justify-center">
+                <h2 class="text-7xl leading-tight text-text-primary font-semibold max-w-4xl text-center">Logging your workouts has never been this easy</h2>
+            </div>
+            <div class="flex flex-col items-center justify-center mt-10">
+                <Button intent="primary" size="lg">Download for Free</Button>
+            </div>
+        </div>
+        <div class="flex items-center justify-center relative w-full h-full">
+            <h3 class="full-width-heading font-bold absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">Workouter</h3>
+            <div class="relative z-2 w-[25vw]">
+                <img src={mockupImage.src} alt="Workouter Mockup" class="w-full h-full object-cover image-shadow" />
+            </div>
+
+        </div>
+    </div>
+</div>
+
+<style>
+    .full-width-heading {
+        font-size: 18vw;
+        background: linear-gradient(to bottom, var(--color-text-secondary), var(--color-text-tertiary));
+        -webkit-background-clip: text;
+        background-clip: text;
+        color: transparent;
+        -webkit-text-fill-color: transparent;
+    }
+
+    .image-shadow {
+        filter: drop-shadow(0 40px 20px rgba(0, 0, 0, 0.45));
+    }
+
+</style>
