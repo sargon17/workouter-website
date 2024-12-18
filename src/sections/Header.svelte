9232d894d8cc2557be370ca61fdf1a483efc78c5
Mykhaylo <63978197+sargon17@users.noreply.github.com>
2024-12-18 22:44:36 +0100

    Add Header component: create a sticky header with title and slot for additional content

---
+++ src/sections/Header.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+    import ThemeSwitcher from "../components/ThemeSwitcher.svelte";
+
+</script>
+
+<div class="sticky top-0 z-1000 py-4">
+    <div class="grid grid-cols-3 max-w-screen-2xl mx-auto">
+        <div class="col-span-1">
+            
+        </div>
+        <div class="col-span-1 flex items-center justify-center">
+            <h1 class="text-2xl font-bold text-text-accent">Workouter</h1>
+        </div>
+        <div class="col-span-1 flex items-center justify-end">
+            <slot />
+        </div>
+    </div>
+</div>
+
+<style>
+
+</style>
