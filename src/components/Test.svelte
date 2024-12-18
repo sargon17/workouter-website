5aa67b7ffe1b86f2f316f9095f5becceec02e953
Mykhaylo <63978197+sargon17@users.noreply.github.com>
2024-12-18 17:06:21 +0100

    testing

--- src/components/Test.svelte
+++ src/components/Test.svelte
@@ -4,4 +4,4 @@
 	}
 </script>
 
-<button onclick={greet} class="bg-blue-500 text-white px-4 py-2 rounded-md">click me</button>
+<button onclick={greet} class="bg-blue-500 text-white px-4 py-2 rounded-md">click me twice</button>
