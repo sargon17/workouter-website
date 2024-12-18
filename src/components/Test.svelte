7ac1282b7587abcdef4028eeab72f397034d05ce
Mykhaylo <63978197+sargon17@users.noreply.github.com>
2024-12-18 22:43:58 +0100

    Remove Test component: deleted Test.svelte file containing a greeting button

--- src/components/Test.svelte
+++
@@ -1,7 +0,0 @@
-<script lang="ts">
-	function greet() {
-		alert('Welcome to Svelte!');
-	}
-</script>
-
-<button onclick={greet} class="bg-blue-500 text-white px-4 py-2 rounded-md">click me</button>
